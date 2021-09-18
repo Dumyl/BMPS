2de110385483f9e15fbab13f4fcf1bc0f654476f
dummy <zmin63683@gmail.com>
2021-09-18 18:13:55 +0630

    bmps

---
+++ recourse/js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('.js--course').waypoint(function(direction){
+        if(direction == 'down'){
+            $('header-0').addClass('sticky');
+        }else{
+            $('header-0').removeClass('sticky');
+        }
+    });
+
+    
+/*scoll nav effect */
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  /* amimation scolll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset: '0%'
+    })
+
+});
